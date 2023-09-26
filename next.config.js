154605aa68802eea5d529223eecf33b5e7c67622
Steven Brown <smbrown1@student.fullsail.edu>
2023-09-26 06:15:45 -0400

    Add github actions script. Configure project for static build.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  distDir: "dist",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

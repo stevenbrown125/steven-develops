ec90c4ecb10cf08aed5bc6ed8a326a47aee82983
Steven Brown <smbrown1@student.fullsail.edu>
2023-10-07 19:24:17 -0400

    Add gradient and max height to background images.

--- frontend/components/SplashImage.tsx
+++ frontend/components/SplashImage.tsx
@@ -27,28 +27,29 @@ export default function SplashImage({
 
   const basePath = "/steven-develops";
   return (
-    <>
+    <div className="">
       <Image
         src={`${basePath}/images/rome-optimized.jpg`}
         fill={true}
         alt="Steven Brown in Rome, Italy"
-        className="-z-10 opacity-0 fade-transition"
+        className="z-0 opacity-0 fade-transition max-h-screen shadow-md"
         id="rome"
       />
       <Image
         src={`${basePath}/images/garda-optimized.jpg`}
         fill={true}
         alt="Garda, Italy"
-        className="-z-10 opacity-0 fade-transition"
+        className="-z-10 opacity-0 fade-transition max-h-screen shadow-md"
         id="garda"
       />
       <Image
         src={`${basePath}/images/campaignia-optimized.jpg`}
         fill={true}
         alt="Campaignia, Italy"
-        className="-z-10 opacity-0 fade-transition"
+        className="-z-10 opacity-0 fade-transition max-h-screen shadow-md"
         id="campaignia"
       />
-    </>
+      <div className="absolute -z-10 inset-x-0 bottom-0 h-1/2 bg-gradient-to-t from-stone-200 to-transparent via-stone-300 pointer-events-none"></div>
+    </div>
   );
 }
